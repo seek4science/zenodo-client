5c300cf4b74f8c09ca8265b2b6567472147d8e24
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-08-17 14:24:29 +0100

    Use sandbox API in tests

--- test/client_test.rb
+++ test/client_test.rb
@@ -4,34 +4,32 @@ class ClientTest < Test::Unit::TestCase
 
   def setup
     @access_token = 'notarealaccesstoken'
+    @uri = 'https://sandbox.zenodo.org/api'
+    @client = Zenodo::Client.new(@access_token, @uri)
   end
 
   def test_client_base_uri_defaults
-    client = Zenodo::Client.new(@access_token)
+    custom_client = Zenodo::Client.new(@access_token)
 
-    assert_equal Zenodo::Client::BASE, client.base.url
+    assert_equal Zenodo::Client::BASE, custom_client.base.url
   end
 
   def test_client_base_uri_can_be_set
     uri = 'https://alpaca-farming-tips.golf'
-    client = Zenodo::Client.new(@access_token, uri)
+    custom_client = Zenodo::Client.new(@access_token, uri)
 
-    assert_equal uri, client.base.url
+    assert_equal uri, custom_client.base.url
   end
 
   def test_list_depositions
-    client = Zenodo::Client.new(@access_token)
-
     VCR.use_cassette('list_depositions') do
-      assert client.list_depositions.is_a?(Array)
+      assert @client.list_depositions.is_a?(Array)
     end
   end
 
   def test_create_deposition
-    client = Zenodo::Client.new(@access_token)
-
     VCR.use_cassette('create_deposition') do
-      deposition = client.create_deposition
+      deposition = @client.create_deposition
 
       assert deposition.is_a?(Zenodo::Deposition)
       assert deposition.id.is_a?(Fixnum)
@@ -40,10 +38,8 @@ class ClientTest < Test::Unit::TestCase
   end
 
   def test_fetch_deposition
-    client = Zenodo::Client.new(@access_token)
-
     VCR.use_cassette('fetch_deposition') do
-      deposition = client.deposition(38762)
+      deposition = @client.deposition(38762)
 
       assert deposition.is_a?(Zenodo::Deposition)
       assert deposition.id.is_a?(Fixnum)
@@ -52,10 +48,8 @@ class ClientTest < Test::Unit::TestCase
   end
 
   def test_create_deposition_file
-    client = Zenodo::Client.new(@access_token)
-
     VCR.use_cassette('create_deposition_file') do
-      deposition = client.deposition(38762)
+      deposition = @client.deposition(38762)
       dummy_data = File.open(File.join('test', 'fixtures', 'dummy_data.csv'))
       deposition_file = deposition.create_file(dummy_data)
 
@@ -66,10 +60,8 @@ class ClientTest < Test::Unit::TestCase
   end
 
   def test_list_deposition_files
-    client = Zenodo::Client.new(@access_token)
-
     VCR.use_cassette('list_deposition_files') do
-      deposition = client.deposition(38762)
+      deposition = @client.deposition(38762)
       deposition_files = deposition.list_files
 
       assert deposition_files.is_a?(Array)
@@ -79,11 +71,10 @@ class ClientTest < Test::Unit::TestCase
   end
 
   def test_fetch_deposition_file
-    client = Zenodo::Client.new(@access_token)
     id = '2876f91e-6427-4c09-b56f-f592fa0f492a'
 
     VCR.use_cassette('fetch_deposition_file') do
-      deposition = client.deposition(38762)
+      deposition = @client.deposition(38762)
       deposition_file = deposition.file(id)
 
       assert deposition_file.is_a?(Zenodo::DepositionFile)
@@ -91,5 +82,36 @@ class ClientTest < Test::Unit::TestCase
       assert deposition_file.details.is_a?(Hash)
     end
   end
+
+  def test_publish_deposition
+    VCR.use_cassette('publish_deposition') do
+      deposition = @client.deposition(1028)
+      assert deposition.publish
+      assert deposition.published?
+    end
+  end
+
+  # TODO: Implement VCRs for the following tests
+  # def test_edit_published_deposition
+  #   @client = Zenodo::Client.new('JptkEVmX94gus8n1BKhfr7QcnaypcOJBFn3YCMqjggvgVZpNdqwCmLZKYkqH', 'https://sandbox.zenodo.org/api')
+  #
+  #   VCR.use_cassette('unlock_published_deposition') do
+  #     deposition = @client.deposition(1028)
+  #     assert deposition.unlock
+  #
+  #     puts deposition.details
+  #   end
+  # end
+  #
+  # def test_discard_published_deposition_changes
+  #   @client = Zenodo::Client.new('JptkEVmX94gus8n1BKhfr7QcnaypcOJBFn3YCMqjggvgVZpNdqwCmLZKYkqH', 'https://sandbox.zenodo.org/api')
+  #
+  #   VCR.use_cassette('discard_deposition_changes') do
+  #     deposition = @client.deposition(1028)
+  #     assert deposition.discard
+  #
+  #     puts deposition.details
+  #   end
+  # end
 
 end
